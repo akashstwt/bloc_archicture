0d4909c52d55b4c8829e33bfb00596693a340a10
akashstwt <akashpatel115347@gmail.com>
2024-10-14 14:34:44 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bloc_archicture/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
