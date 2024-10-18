6ff8128e3cd8b74b41a826a97008c82ce72ad122
akashstwt <akashpatel115347@gmail.com>
2024-10-18 16:25:58 +0530

    inc_dec updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,25 +1,20 @@
 import 'package:bloc_archicture/cubit/counter_cubit.dart';
+import 'package:bloc_archicture/inc_dec_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final counterCubit = CounterCubit();
-
-  @override
   Widget build(BuildContext context) {
-    final counter = counterCubit.state;
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -28,20 +23,26 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            BlocBuilder<CounterCubit, int>(
+                bloc: counterCubit,
+                builder: (context, counter) {
+                  return Text(
+                    '$counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          counterCubit.increment();
-          setState(() {});
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const IncDecPage(),
+            ),
+          );
         },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.navigation),
       ),
     );
   }
